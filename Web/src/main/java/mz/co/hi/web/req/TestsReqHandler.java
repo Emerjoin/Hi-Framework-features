60794d942c2a366acb12c8d85e6a3a6d2fd7da43
Mário Júnior <francisco.junior.mario@gmail.com>
2017-01-30 05:59:09 +0200

    TestsReqHandler refactored

--- Web/src/main/java/mz/co/hi/web/req/TestsReqHandler.java
+++ Web/src/main/java/mz/co/hi/web/req/TestsReqHandler.java
@@ -22,42 +22,27 @@ public class TestsReqHandler extends ReqHandler {
 
     @Override
     public boolean handle(RequestContext requestContext) throws ServletException, IOException {
-
-
         if(!requestContext.getRouteUrl().equals("test"))
             return false;
-
-
         URL testsHtml = requestContext.getServletContext().getResource("/webroot/tests/run.html");
         URL testsJs = requestContext.getServletContext().getResource("/webroot/tests/run.js");
 
-        if(testsHtml==null||testsJs==null){
-
+        if(testsHtml==null||testsJs==null)
             throw new HiException("Could not invoke tests runner. Make sure the files /webroot/tests/run.html AND /webroot/tests/run.js are BOTH present");
-
-        }
-
 
         String hi_test_js = "<script src=\"hi-es5-tests.js\"></script>\n";
         String testsHtmlContent = Helper.readLines(testsHtml.openStream(),null);
         String bodyClose = "</body>";
 
-
         StringBuilder scriptTags = new StringBuilder();
         Map<String,Boolean> testFiles = AppConfigurations.get().getTestFiles();
-        Set<String> testFilesSET = testFiles.keySet();
-
-        for(String testFile : testFilesSET){
-
+        Set<String> testFilesSet = testFiles.keySet();
+        for(String testFile : testFilesSet)
             scriptTags.append("<script src =\"test/"+testFile+"\"></script>\n");
-
-        }
 
         String bootstrapSnipet = "<script>Hi.$angular.run();</script>\n<script>angular.bootstrap($(document),['hi']);</script>\n";
         String appendedScript = hi_test_js+bootstrapSnipet+scriptTags;
-
         testsHtmlContent = testsHtmlContent.replace(bodyClose,appendedScript+bodyClose+"\n");
-
         requestContext.echo(testsHtmlContent);
         return true;
 
