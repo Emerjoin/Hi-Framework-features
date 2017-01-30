aa206d80af0c80da35cf2267392e73f644d24f46
Mário Júnior <francisco.junior.mario@gmail.com>
2017-01-30 05:50:59 +0200

    WebConfig class refactored

--- Web/src/main/java/mz/co/hi/web/config/xml/sections/WebConfig.java
+++ Web/src/main/java/mz/co/hi/web/config/xml/sections/WebConfig.java
@@ -17,38 +17,9 @@ public class WebConfig implements Configurator {
 
     public static final String DEFAULT_VIEWS_DIRECTORY = "views";
 
-    @Override
-    public void doConfig(AppConfigurations configs, Map<String, org.w3c.dom.Element> elements, org.w3c.dom.Element docElement) throws BadConfigException {
 
-        //Template and MVC Configurations
-        org.w3c.dom.Element webElement = (org.w3c.dom.Element) docElement.getElementsByTagName("web").item(0);
-
-        NodeList viewsDirectoryNodes = webElement.getElementsByTagName("views-directory");
-        if(viewsDirectoryNodes.getLength()>0){
-
-            org.w3c.dom.Element viewsDirectoryElement =(org.w3c.dom.Element) viewsDirectoryNodes.item(0);
-            String viewsDirectory = viewsDirectoryElement.getTextContent();
-            configs.setViewsDirectory(viewsDirectory);
-
-        }else{
-
-            configs.setViewsDirectory(DEFAULT_VIEWS_DIRECTORY);
-
-        }
-
-        NodeList welcomeUrlNodes = webElement.getElementsByTagName("welcome-url");
-        if(welcomeUrlNodes.getLength()>0){
-
-            org.w3c.dom.Element welcomeUrlElement =(org.w3c.dom.Element) welcomeUrlNodes.item(0);
-            String wecomeUrl = welcomeUrlElement.getTextContent();
-            configs.setWelcomeUrl(wecomeUrl);
-
-
-        }
-
-
+    private void templates(Element webElement, AppConfigurations configs){
         NodeList templatesNodes = webElement.getElementsByTagName("templates");
-
         String[] templates = null;
 
         if(templatesNodes.getLength()>0){
@@ -65,35 +36,53 @@ public class WebConfig implements Configurator {
             }
 
         }else templates=new String[]{"index"};
-
-
         configs.setTemplates(templates);
 
+    }
 
+
+    private void deploymentAndLang(Element docElement, AppConfigurations configs){
         NodeList deploymentModeNodeList = docElement.getElementsByTagName("deployment-mode");
+
         if(deploymentModeNodeList.getLength()>0){
-
-
             String deploymentModevalue = deploymentModeNodeList.item(0).getTextContent();
             if(deploymentModevalue.equals("PRODUCTION"))
                 configs.setDeploymentMode(AppConfigurations.DeploymentMode.PRODUCTION);
-
-
-
         }
 
         NodeList defaultLangNode = docElement.getElementsByTagName("default-lang");
         if(defaultLangNode.getLength()>0){
-
             org.w3c.dom.Element defaultLangElement = (org.w3c.dom.Element) defaultLangNode.item(0);
             configs.setDefaultLanguage(defaultLangElement.getTextContent());
+        }
 
+    }
 
+    @Override
+    public void doConfig(AppConfigurations configs, Map<String, org.w3c.dom.Element> elements, org.w3c.dom.Element docElement) throws BadConfigException {
+
+        //Template and MVC Configurations
+        org.w3c.dom.Element webElement = (org.w3c.dom.Element) docElement.getElementsByTagName("web").item(0);
+        NodeList viewsDirectoryNodes = webElement.getElementsByTagName("views-directory");
+        if(viewsDirectoryNodes.getLength()>0){
+            org.w3c.dom.Element viewsDirectoryElement =(org.w3c.dom.Element) viewsDirectoryNodes.item(0);
+            String viewsDirectory = viewsDirectoryElement.getTextContent();
+            configs.setViewsDirectory(viewsDirectory);
+        }else configs.setViewsDirectory(DEFAULT_VIEWS_DIRECTORY);
+
+        NodeList welcomeUrlNodes = webElement.getElementsByTagName("welcome-url");
+        if(welcomeUrlNodes.getLength()>0){
+            org.w3c.dom.Element welcomeUrlElement =(org.w3c.dom.Element) welcomeUrlNodes.item(0);
+            String wecomeUrl = welcomeUrlElement.getTextContent();
+            configs.setWelcomeUrl(wecomeUrl);
         }
 
 
+       templates(webElement,configs);
+       deploymentAndLang(docElement,configs);
 
     }
+
 
 
 
